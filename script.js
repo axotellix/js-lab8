2ea318343e4b2052c40dedeb7da8a0db8f2a7f52
axotellix <RSAndrey@list.ru>
2020-12-08 09:41:41 +0300

    Выполнено задание 1

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 
+/* [ TASK 1 > Sheep counting ] */
+let c = 0;
+let n = prompt('Введите кол-во овец:');
+
+if(n > 100) {
+    console.log('Вы спать вообще собираетесь?')
+} else {
+    // start > counting
+    console.group('Задание 1. Считаем овец');
+
+    for(let i = 1; i <= n; i++) {
+        console.log(`${i} овечка`);
+    }
+
+    console.groupEnd();
+}
+
+
+
 
